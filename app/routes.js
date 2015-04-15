7b97ed3ea6ee201bef469afdfdb7be91524f3118
Colm Britton <colmjude@gmail.com>
2015-04-15 15:33:59 +0100

    make listing into a template

--- app/routes.js
+++ app/routes.js
@@ -1,3 +1,6 @@
+var fs = require("fs");
+var path = require('path');
+
 module.exports = {
   bind : function (app) {
 
@@ -5,6 +8,18 @@ module.exports = {
       res.render('index');
     });
 
+
+    app.get('/jobs/search_by_skill', function (req, res) {
+      var data;
+      fs.readFile(path.join(__dirname, 'data/listings.json'), {encoding: 'utf-8'}, function (err, data) {
+          if (err) {
+              throw err;
+          } else {
+            data = JSON.parse(data);
+          }
+          res.render('jobs/search_by_skill', {listings: data});
+      });
+    });
     // add your routes here
 
   }
